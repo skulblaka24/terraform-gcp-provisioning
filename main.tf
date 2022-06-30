0e3fce22150c86cf30daaa40251bcc0b78e2b1df
Gauthier <gauthier.donikian@gmail.com>
2022-06-30 18:18:50 +0200

    Adding Service Account auth

--- main.tf
+++ main.tf
@@ -3,36 +3,12 @@ terraform {
     google = {
       source  = "hashicorp/google"
     }
-
-    doormat = {
-      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
-      version = "~> 0.0.2"
-    }
-  }
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "gauth"
-
-    workspaces {
-      name = "terraform-gcp-provisioning"
-    }
-  }
-}
-
-provider "doormat" {}
-
-data "doormat_gcp_credentials" "creds" {
-  provider = doormat
-
-  service_account = var.service_account_email
-  project_id      = var.project_name
 }
 
 # Configure the Google Cloud provider
 provider "google" {
   region  = var.region
   project = var.project_name
-  access_token = data.doormat_gcp_credentials.creds.access_token
 }
 
 module "vm_compute_instance" {
