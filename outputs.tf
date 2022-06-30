8cb61dfb5b6c30cee82dbc9c2919969480b9af84
Gauthier <gauthier.donikian@gmail.com>
2022-06-30 19:20:46 +0200

    Adding output and dependance

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 # Print out the tfe node IPs.
 output "gcp_instances_vm_ip" {
-  value = vm_compute_instance.google_compute_instance.tfe.*.network_interface.0.access_config.0.nat_ip
+  value = module.vm_compute_instance.google_compute_instance.tfe.*.network_interface.0.access_config.0.nat_ip
 }
