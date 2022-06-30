215a99e2373a6017ed6855a0a7739ab5944b88a7
Gauthier <gauthier.donikian@gmail.com>
2022-06-30 18:00:14 +0200

    Adding Doormat auth

--- main.tf
+++ main.tf
@@ -1,12 +1,41 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+    }
+
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.2"
+    }
+  }
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "gauth"
+
+    workspaces {
+      name = "terraform-gcp-provisioning"
+    }
+  }
+}
+
+provider "doormat" {}
+
+data "doormat_gcp_credentials" "creds" {
+  provider = doormat
+
+  service_account = var.service_account_email
+  project_id      = var.project_name
+}
+
 # Configure the Google Cloud provider
 provider "google" {
   region  = var.region
   project = var.project_name
-  access_token = "sdfsd"
+  access_token = data.doormat_gcp_credentials.creds.access_token
 }
 
 module "vm_compute_instance" {
-#  source  = "./modules/terraform-module-gcp-provisioning"
   source  = "github.com/skulblaka24/terraform-module-gcp-compute"
 
   region_zone = var.region_zone
